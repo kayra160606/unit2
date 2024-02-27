1ad63d6ad9f0b07ef6b14a2458e55be0be4e3cfc
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-02-27 19:21:42 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,31 @@
+public class sort
+ {
+    public static void main(String[] args) 
+    {
+        int i,j,n,temp;
+        n=args.length;
+        int a[]=new int[n];
+        for(i=0;i<n;i++) 
+        {
+            a[i]=Integer.parseInt(args[i]);
+        }
+        for(i=0;i<n;i++) 
+        {
+            for(j=i+1;j<n;j++) 
+            {
+                if(a[i]>a[i])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        } 
+        System.out.println("Array Element after sorting:");
+        for(i=0;i<n;i++) 
+        {
+            System.out.println(a[i]);
+        }
+        
+    }
+}
